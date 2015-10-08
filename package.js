4057df85c16ed8898315c46af27219ddb2bce615
Curtis Maddalozzo <cmaddalozzo@gmail.com>
2015-10-08 09:43:12 -0700

    Use node-stripe v4.

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
-	summary: "Stripe.js and Node-Stripe brought to Meteor.",
-	version: "2.1.0",
-	name: "mrgalaxy:stripe",
-	git: "https://github.com/tyler-johnson/stripe-meteor.git"
+  summary: "Stripe.js and Node-Stripe v4 brought to Meteor. Based on mrgalaxy:stripe",
+	version: "1.0.0",
+	name: "leisure:stripe",
+	git: "https://github.com/cmaddalozzo/stripe-meteor.git"
 });
 
-Npm.depends({ "stripe": "3.2.0" });
+Npm.depends({ "stripe": "4.0.0" });
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.0.1');
@@ -16,7 +16,7 @@ Package.onUse(function(api) {
 });
 
 Package.on_test(function(api) {
-	api.use(['tinytest','mrgalaxy:stripe']);
+	api.use(['tinytest','leisure:stripe']);
 	api.add_files([ "tests/client.js", "tests/checkout.js" ], 'client');
 	api.add_files([ "tests/server.js" ], 'server');
 });
